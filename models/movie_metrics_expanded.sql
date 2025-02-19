eeeae7d7fb415df1cb470523a5da778b2cc3bea0
Armandas Lidžius <armandaslid@gmail.com>
2025-02-19 15:22:11 +0000

    Some changes

---
+++ models/movie_metrics_expanded.sql
@@ -0,0 +1,41 @@
+WITH tt_i AS
+(
+SELECT
+    movie_id
+    ,month
+    ,SUM(total_invoice_sum) AS rental_cost
+    ,location_id AS location
+FROM
+    {{ source('silverscreen', 'invoices') }}
+GROUP BY
+    movie_id, month, location
+),
+
+tt AS
+(
+SELECT
+    mm.movie_id
+    ,mm.month
+    ,SUM(mm.tickets_sold)
+    ,SUM(i.rental_cost)
+    ,mm.location
+    ,SUM(mm.transaction_total - i.rental_cost) AS revenue
+FROM
+    {{ ref('movie_metrics') }} AS mm
+JOIN
+    tt_i AS i
+ON
+    mm.movie_id = i.movie_id
+AND
+    mm.month = i.month
+AND
+    mm.location = i.location
+GROUP BY
+    mm.movie_id, mm.month, mm.location
+)
+
+SELECT
+    *
+FROM
+    tt
+ORDER BY revenue ASC
