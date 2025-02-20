0d784b2b9ef2a99e03bed63ace97f1dba4e78640
Armandas Lidžius <armandaslid@gmail.com>
2025-02-20 21:22:50 +0000

    Added a comment to a date_trunc_month.sql

--- macros/date_trunc_month.sql
+++ macros/date_trunc_month.sql
@@ -1,4 +1,4 @@
--- Macro to truncate dates/timestamps to month level
+-- Macro to truncate dates/timestamps to month level (This one is for Snowflake, BigQuery has different syntax)
 
 {% macro date_trunc_month(date_column, date_part='MONTH') %}
 
